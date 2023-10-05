88ecc371fbc599d45690546f6a49620c16e2c37f
ayush-raj13 <ayushraj.iiitbh@gmail.com>
2023-10-05 17:23:49 +0530

    Contributors Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Contributors from "./components/Contributors";
 import Home from "./components/home";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="dark:bg-gray-900 dark:text-white">
-      <Home />
+      <Routes>
+        <Route path="/RobustKey-PasswordGenerator" element={<Home />} />
+        <Route path="/contributors" element={<Contributors />} />
+      </Routes>
     </div>
   );
 }
