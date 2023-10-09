52e7e55cb5f125e101e33b37481cd1642f6af82c
Aman Raj Rana <mail2amanrana@gmail.com>
2023-10-09 09:33:11 +0530

    Ref: Required changes for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 
-
 function App() {
   return (
     <div className="dark:bg-gray-900 dark:text-white">
       <Header />
       <Routes>
-        <Route path="/RobustKey-PasswordGenerator" element={<Home />} />
-        <Route path="RobustKey-passwordGenerator/contributors" element={<Contributors />} />
-        <Route path="/RobustKey-PasswordGenerator/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contributors" element={<Contributors />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
