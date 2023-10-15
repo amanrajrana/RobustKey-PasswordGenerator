db06af051a7540365e89e7914033b7565ea493ed
Aman Raj Rana <mail2amanrana@gmail.com>
2023-10-15 15:45:48 +0530

    Introduce New theme toggle button

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./contexts/theme/themeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
